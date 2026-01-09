17937e7e4748bf66752df655b3a30ceb73d22457
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2026-01-09 09:56:23 +0100

    change role

--- app/models/role.rb
+++ app/models/role.rb
@@ -26,6 +26,7 @@ class Role < ApplicationRecord
     end
   end
 
+
   has_and_belongs_to_many :users, join_table: :users_roles # rubocop:disable Rails/HasAndBelongsToMany
 
   belongs_to :resource,
