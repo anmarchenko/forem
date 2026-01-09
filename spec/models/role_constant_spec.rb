8535376a5a0688137e234eb7e258b3deabe66fdf
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2026-01-09 12:24:07 +0100

    a new test

--- spec/models/role_constant_spec.rb
+++ spec/models/role_constant_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Role do
     it "contains the value" do
       expect(described_class::ROLES).to include("codeland_admin")
     end
+
+    it "has NEW constant" do
+      expect(Constants::Role::NEW).to eq(0)
+    end
   end
 end
