484e4c56f5a237e3dd03f591b073cd309cd01230
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2026-06-15 12:10:26 +0200

    Validate ddtest discovery cache invalidation

--- spec/ddtest/excluded/exclude_pattern_canary_spec.rb
+++ spec/ddtest/excluded/exclude_pattern_canary_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Touch this spec-tree file when validating DDTest discovery-cache invalidation.
 RSpec.describe "DDTest exclude pattern canary" do
   it "would be discovered without the ddtest exclude pattern" do
     expect(true).to be(true)
